daa240c2095eabc8abfa3f66c0a30d2f382aecfd
MarianyLopez <marlpez111@misena.edu.co>
2023-10-20 18:32:42 -0500

    Cors configuration

--- src/main/java/com/api/messengerservice/MessengerServiceApplication.java
+++ src/main/java/com/api/messengerservice/MessengerServiceApplication.java
@@ -2,11 +2,23 @@ package com.api.messengerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MessengerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessengerServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://api-rest-messenger-service-production.up.railway.app").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 }
